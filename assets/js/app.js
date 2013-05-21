cf918c89733625adc50caf21199a8abecc9320c7
Julia Teitelbaum <julialt@gmail.com>
2013-05-21 01:12:43 -0400

    created html pages for chart creation ui and tweaked behavior scatterplot a little

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-$('#example').popover();
+/* from http://mobile.tutsplus.com/tutorials/mobile-web-apps/remove-address-bar/ */
+function hideAddressBar()
+{
+    if(!window.location.hash)
+    {
+        if(document.height <= window.outerHeight + 10)
+        {
+            document.body.style.height = (window.outerHeight + 50) +'px';
+            setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+        }
+        else
+        {
+            setTimeout( function(){ window.scrollTo(0, 1); }, 0 );
+        }
+    }
+}
+window.addEventListener("load", hideAddressBar );
+window.addEventListener("orientationchange", hideAddressBar );
