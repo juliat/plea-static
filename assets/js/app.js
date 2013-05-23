931ea453afd922da6ce05edb68cecb42ab0a1087
Julia Teitelbaum <julialt@gmail.com>
2013-05-23 12:53:23 -0400

    fixed click lag issue on ipad by getting rid of hover state styles, which cause click delays on iOS

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,10 +17,15 @@ function hideAddressBar()
 
 $(document).ready(function() {
     // bind logger labels
-    $('.logger label').on('touchend', function(){
-            console.log('touch');
+    /*$('.logger .label').on('click', function(e){ */
+    $('.logger .label').fastClick(function(e) { 
+            // alert('fast clicked!');
+            // e.preventDefault();
+            // console.log('touch');
             $(this).toggleClass('active');
+            // return false;
     });
+
 
     // bind address bar stuff
     window.addEventListener("load", hideAddressBar );
